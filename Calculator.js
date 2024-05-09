4e464fa96d76eb6032a64efb0a619d088855675a
Krishnan0501 <krishnan5312002@gmail.com>
2024-05-09 10:00:43 +0530

    Calculator JavaScript Files Added

--- Calculator.js
+++ Calculator.js
@@ -1,1 +1,15 @@
-//Calculator javascript file 
+//Calculator javascript file
+const display = document.getElementById("display");
+function appendToDisplay(input) {
+  display.value += input;
+}
+function ClearDisplay() {
+  display.value = "";
+}
+function Calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
